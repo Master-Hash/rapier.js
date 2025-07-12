f3fdccdaaa1b4ac533de4a7298b94a425d0e3dd8
Master-Hash <hash@hash.moe>
2025-07-13 01:41:13 +0800

    玩票：多线程
    sleepy no message((

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@
 #![allow(non_snake_case)] // JS uses camelCase, so we will follow its convention for the generated bindings.
                           // #![deny(missing_docs)]
 
+pub use wasm_bindgen_rayon::init_thread_pool;
+
 extern crate nalgebra as na;
 #[cfg(feature = "dim2")]
 extern crate rapier2d as rapier;
